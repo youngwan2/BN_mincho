1480502466ef2700a96e8fc4f8684a3c7e7141e9
Kim Young Wan <qodna25@gmail.com>
2025-05-21 12:47:24 +0900

    refactor: MemberEntity 이름을 UserEntity로 í통일

---
+++ src/main/java/com/mincho/herb/domain/dashboard/api/DashboardController.java
@@ -0,0 +1,4 @@
+package com.mincho.herb.domain.dashboard.api;
+
+public class DashboardController {
+}
---
+++ src/main/java/com/mincho/herb/domain/dashboard/application/DashboardServiceImpl.java
@@ -0,0 +1,4 @@
+package com.mincho.herb.domain.dashboard.application;
+
+public class DashboardServiceImpl {
+}
